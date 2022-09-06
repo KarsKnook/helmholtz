dfdf9cb239bb9dd4c10b5d1da3537cc099d92bf2
Kars Knook <kk519@ic.ac.uk>
2022-09-06 14:42:02 +0100

    RHS in apply()

--- preconditioning.py
+++ preconditioning.py
@@ -4,9 +4,9 @@ from firedrake import dmhooks
 import matplotlib.pyplot as plt
 
 
-def build_problem(mesh_size, parameters, k, epsilon):
+def build_problem_sin2(mesh_size, parameters, k, epsilon, epsilon_0=0):
     """
-    Original (non-shifted) problem
+    Original (non-shifted) problem for u = sin^2(pi*x)sin^2(pi*y) on UnitSquareMesh
     """
     mesh = fd.UnitSquareMesh(mesh_size, mesh_size)
 
@@ -21,6 +21,40 @@ def build_problem(mesh_size, parameters, k, epsilon):
     x, y = fd.SpatialCoordinate(mesh)
     f = (-k**2*fd.sin(fd.pi*x)**2*fd.sin(fd.pi*y)**2
          + fd.pi**2*(fd.cos(2*fd.pi*(x+y)) + fd.cos(2*fd.pi*(x-y)) - fd.cos(2*fd.pi*x) - fd.cos(2*fd.pi*y)))
+
+    # linear variational form of original problem
+    a = (fd.inner(fd.Constant(-1j*k)*sigma, tau)*fd.dx
+         - fd.inner(fd.grad(u), tau)*fd.dx
+         + fd.inner(sigma, fd.grad(v))*fd.dx
+         + fd.inner(fd.Constant(-1j*k)*u, v)*fd.dx
+         + fd.inner(u, v)*fd.ds)
+    L = - fd.inner(f/fd.Constant(+1j*k), v)*fd.dx
+
+    # setting up a linear variational solver and passing in k, epsilon and f in appctx
+    appctx = {"k": k, "epsilon": epsilon, "f": f}
+    w = fd.Function(W)
+    vpb = fd.LinearVariationalProblem(a, L, w)
+    solver = fd.LinearVariationalSolver(vpb, solver_parameters=parameters, appctx=appctx)
+
+    return solver, w
+
+
+def build_problem_point_source(mesh_refinement, parameters, k, epsilon, epsilon_0=0):
+    """
+    Wavemaker on UnitDiskMesh
+    """
+    mesh = fd.UnitDiskMesh(mesh_refinement)
+
+    V = fd.VectorFunctionSpace(mesh, "DG", degree=1, dim=2)
+    Q = fd.FunctionSpace(mesh, "CG", degree=2)
+    W = V * Q
+
+    sigma, u = fd.TrialFunctions(W)
+    tau, v = fd.TestFunctions(W)
+
+    # RHS for u = sin^2(pi*x)sin^2(pi*y)
+    x, y = fd.SpatialCoordinate(mesh)
+    f = fd.conditional(fd.le(x**2+y**2, 0.01), 1, 0)
 
     # linear variational form of original problem
     a = (fd.inner(fd.Constant(-1j*k)*sigma, tau)*fd.dx
@@ -55,8 +89,9 @@ class pHSS_PC(fd.preconditioners.base.PCBase):
         self.context = context
 
         # obtaining k, epsilon and f
-        k = context.appctx.get("k", 1.0)
-        epsilon = context.appctx.get("epsilon", 1.0)
+        k = context.appctx.get("k")
+        self.k = k
+        epsilon = context.appctx.get("epsilon")
         f = context.appctx.get("f")
 
         # initiliazing test and trial functions
@@ -109,8 +144,9 @@ class pHSS_PC(fd.preconditioners.base.PCBase):
         self.its = PETSc.Options().getInt(options_prefix + "its")
 
         w_old = fd.Function(W)
+        self.w_old = w_old
         sigma_old, u_old = w_old.split()
-        self.hss_rhs = (fd.Constant((k-1)/(-2*k))
+        self.hss_rhs = (fd.Constant((k-1)/(k+1))
                         *(fd.inner(fd.Constant((epsilon+1j)*k)*sigma_old, tau)*fd.dx
                           + fd.inner(fd.grad(u_old), tau)*fd.dx
                           - fd.inner(sigma_old, fd.grad(v))*fd.dx
@@ -124,17 +160,18 @@ class pHSS_PC(fd.preconditioners.base.PCBase):
         """
         Copied from asQ/asQ/diag_preconditioner.py
         """
+        k = fd.Constant(self.k)
         #first solve
         with self.w.dat.vec_wo as w_:
             self.ksp.solve(X, w_)  # b = inner(f, v) is the only RHS term because x^0 = 0
 
         #all other solves
         for i in range(self.its - 1):
-            sigma_old, u_old = self.w.split()
+            self.w_old.assign(self.w)
             fd.assemble(self.hss_rhs, form_compiler_parameters=self.context.fc_params, tensor=self.q)
 
             with self.w.dat.vec_wo as w_, self.q.dat.vec_ro as q_:
-                self.ksp.solve(q_ + X, w_)  # corresponds to self.hss_rhs + inner(f,v)
+                self.ksp.solve(q_ + 2*k/(k+1)*X, w_)  # corresponds to self.hss_rhs + inner(f,v)
         
         #copy the result into Y
         with self.w.dat.vec_ro as w_:
@@ -164,14 +201,15 @@ class Schur(fd.AuxiliaryOperatorPC):
 
 
 #testing the preconditioner
-n = 10
-k = 2
+n = 3
+k = 10
 epsilon = 2
 
 parameters = {
     "ksp_type": "gmres",
     "ksp_gmres_restart": 100,
     #"ksp_atol": 1e-9,
+    #"ksp_max_it": 1000,
     "pc_type": "python",
     "pc_python_type": __name__ + ".pHSS_PC",
     "helmhss_ksp_type": "preonly",
@@ -185,17 +223,50 @@ parameters = {
     "helmhss_fieldsplit_1_pc_python_type": __name__ + ".Schur",
     "helmhss_fieldsplit_1_aux_pc_type": "lu",
     "helmhss_mat_type": "nest",
-    "helmhss_its": 2,
+    "helmhss_its": 10*k,
     "mat_type": "matfree",
     "ksp_monitor": None,
     "ksp_converged_reason": None,
     #"ksp_view": None,
 }
 
-solver, w = build_problem(n, parameters, k, epsilon)
+solver, w = build_problem_point_source(n, parameters, k, epsilon)
 solver.solve()
 
 sigma, u = w.split()
 collection = fd.tripcolor(u, cmap='coolwarm')
 plt.colorbar(collection)
 plt.show()
+
+"""for k in range(1, 20):
+    n = 4
+    epsilon = 0.1
+
+    parameters = {
+        "ksp_type": "gmres",
+        "ksp_gmres_restart": 100,
+        #"ksp_atol": 1e-9,
+        #"ksp_max_it": 1000,
+        "pc_type": "python",
+        "pc_python_type": __name__ + ".pHSS_PC",
+        "helmhss_ksp_type": "preonly",
+        "helmhss_pc_type": "fieldsplit",
+        "helmhss_pc_fieldsplit_type": "schur",
+        "helmhss_pc_fieldsplit_schur_fact_type": "full",
+        "helmhss_fieldsplit_0_ksp_type": "preonly",
+        "helmhss_fieldsplit_0_pc_type": "ilu",
+        "helmhss_fieldsplit_1_ksp_type": "preonly",
+        "helmhss_fieldsplit_1_pc_type": "python",
+        "helmhss_fieldsplit_1_pc_python_type": __name__ + ".Schur",
+        "helmhss_fieldsplit_1_aux_pc_type": "lu",
+        "helmhss_mat_type": "nest",
+        "helmhss_its": 1,
+        "mat_type": "matfree",
+        #"ksp_monitor": None,
+        #"ksp_converged_reason": None,
+        #"ksp_view": None,
+    }
+
+    solver, w = build_problem_point_source(n, parameters, k, epsilon)
+    solver.solve()
+    print(k, solver.snes.ksp.getIterationNumber())"""
